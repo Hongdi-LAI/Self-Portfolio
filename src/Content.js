18f5491c93dbb4e2742994777557e59e984b0a21
Hongdi-LAI <69794567+Hongdi-LAI@users.noreply.github.com>
2020-09-20 14:16:27 +0200

    Portfolio Structure Update

---
+++ src/Content.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Content.css'
+
+function Content() {
+    return (
+        <div className = "content">
+            <div className = "content__header"></div>
+            <div className = "content__main"></div>
+        </div>
+    )
+}
+
+export default Content
